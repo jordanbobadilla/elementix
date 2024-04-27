bde63740c914259fd8d37efb42c0a9269caf5e81
Jordan <jordanboba08@gmail.com>
2024-04-26 19:25:10 -0700

    Add Foreground Priority to the HomePage Texts

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] 
         bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"
         />
-        <p className="text-center">Run your agency, in one place</p>
+        <p className="text-center z-10">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-7xl font-bold text-center md:text-[150px]">
             Elementix
