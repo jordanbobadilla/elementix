2363fe63c430eb0ef58bc7e833eceba91c739902
Jordan <jordanboba08@gmail.com>
2024-04-28 01:45:58 -0700

    Create Upload Thing Library

---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,7 @@
+import { generateUploadButton, generateUploadDropzone, generateUploader, generateReactHelpers } from "@uploadthing/react"
+import { OurFileRouter } from "@/app/api/uploadthing/core"
+
+export const UploadButton = generateUploadButton<OurFileRouter>()
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
+export const Uploader = generateUploader<OurFileRouter>()
+export const {useUploadThing, uploadFiles} = generateReactHelpers<OurFileRouter>()
