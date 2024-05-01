bbd278c8ec7736c5afcb6db62f7f5c46525c12a8
Jordan <jordanboba08@gmail.com>
2024-04-30 20:02:22 -0700

    Add Menu Options App Router Pages

---
+++ src/app/(main)/agency/[agencyId]/billing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>Billing</div>
+  )
+}
+
+export default Page
---
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>Team</div>
+  )
+}
+
+export default Page
