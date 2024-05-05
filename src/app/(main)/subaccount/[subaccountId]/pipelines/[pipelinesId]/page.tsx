9af4e4031ca3faaf5b16245b684ce9c2fffabea4
Jordan <jordanboba08@gmail.com>
2024-05-05 16:20:20 -0700

    Add Tabs to Pipelines Pages

--- src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
@@ -1,40 +1,58 @@
-import { Tabs, TabsList } from '@/components/ui/tabs'
-import { db } from '@/lib/db'
-import { getLanesWithTicketsAndTags, getPipelinesDetails } from '@/lib/queries'
-import { LaneDetail } from '@/lib/types'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import PipelinesInfobar from '../_components/pipelines-infobar'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { db } from "@/lib/db";
+import { getLanesWithTicketsAndTags, getPipelinesDetails } from "@/lib/queries";
+import { LaneDetail } from "@/lib/types";
+import { redirect } from "next/navigation";
+import React from "react";
+import PipelinesInfobar from "../_components/pipelines-infobar";
 
 type Props = {
   params: {
-    subaccountId: string
-    pipelinesId: string
-  }
-}
+    subaccountId: string;
+    pipelinesId: string;
+  };
+};
 
-const PipelinesPage = async ({params}: Props) => {
-  const pipelinesDetails = await getPipelinesDetails(params.pipelinesId)
+const PipelinesPage = async ({ params }: Props) => {
+  const pipelinesDetails = await getPipelinesDetails(params.pipelinesId);
 
   if (!pipelinesDetails) {
-    return redirect(`/subaccount/${params.subaccountId}/pipelines`)
+    return redirect(`/subaccount/${params.subaccountId}/pipelines`);
   }
 
   const pipelines = await db.pipeline.findMany({
-    where: { subAccountId: params.subaccountId}
-  })
+    where: { subAccountId: params.subaccountId },
+  });
 
   const lanes = (await getLanesWithTicketsAndTags(
     params.pipelinesId
-  )) as LaneDetail[]
+  )) as LaneDetail[];
 
   return (
-    <Tabs defaultValue='view' className='w-full'>
-      <TabsList className='bg-transparent border-b-2 h-16 w-full justify-between mb-4'>
-        <PipelinesInfobar subAccountId={params.subaccountId} pipelines={pipelines} pipelineId={params.pipelinesId}/>
+    <Tabs defaultValue="view" className="w-full">
+      <TabsList className="bg-transparent border-b-2 h-16 w-full justify-between mb-4">
+        <PipelinesInfobar
+          subAccountId={params.subaccountId}
+          pipelines={pipelines}
+          pipelineId={params.pipelinesId}
+        />
+        <div>
+          <TabsTrigger value="view" className="!bg-transparent">
+            Pipeline View
+          </TabsTrigger>
+          <TabsTrigger value="settings" className="!bg-transparent">
+            Settings
+          </TabsTrigger>
+        </div>
       </TabsList>
+      <TabsContent value="view">
+        Pipeline View
+      </TabsContent>
+      <TabsContent value="settings">
+        Settings View
+      </TabsContent>
     </Tabs>
-  )
-}
+  );
+};
 
-export default PipelinesPage
+export default PipelinesPage;
